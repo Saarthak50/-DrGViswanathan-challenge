9acd76fa6905df2c539c31795d12df030870f45c
Sarthak Kumar Yadav <ysarthakyadav3737@gmail.com>
2026-06-12 18:29:28 +0530

    day05q1s2

--- validperfectsquare.java
+++ validperfectsquare.java
@@ -8,3 +8,25 @@ class Solution{ //approach 1 .
         return num==0;
     }
 }
+
+class Solution2 {
+    public boolean isPerfectSquare(int num) {
+        long left = 1;
+        long right = num;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
